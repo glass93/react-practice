9e3c0ba3e3bc1bcbd5af05a1440129919228ba1c
한상정 <hsj7630@daum.net>
2021-10-12 20:37:02 +0900

    iteration

---
+++ 06_iteration/iteration/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import IterationSample from './IterationSample';
+import IterationSample2 from './IterationSample2';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <IterationSample /> */}
+        <IterationSample2 />
+      </div>
+    );
+  }
+}
+
+export default App;
