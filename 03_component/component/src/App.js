8819199ce8bc55215e0ca4d4b952602e821746cf
한상정 <hsj7630@daum.net>
2021-10-12 15:52:04 +0900

    state

--- 03_component/component/src/App.js
+++ 03_component/component/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
 
 const App = () => {
   return (
-    <MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>
+    <Say />
+    // <Counter />
+    // <MyComponent name="React" favoriteNumber={1}>
+    //   리액트
+    // </MyComponent>
   );
 };
 
