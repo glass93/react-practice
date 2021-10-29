329963641fd84b9e34878afe265fdd11024b786c
한상정 <hsj7630@daum.net>
2021-10-29 18:19:50 +0900

    redux middleware

--- 18_redux_middleware/src/modules/index.js
+++ 18_redux_middleware/src/modules/index.js
@@ -1,12 +1,18 @@
+import { all } from '@redux-saga/core/effects';
 import { combineReducers } from 'redux';
-import counter from './counter';
+import counter, { counterSaga } from './counter';
 import loading from './loading';
-import sample from './sample';
+import sample, { sampleSaga } from './sample';
 
 const rootReducer = combineReducers({
   counter,
   sample,
   loading,
 });
+
+export function* rootSaga() {
+  // all 함수는 여러 사가를 합쳐 주는 역할을 함
+  yield all([counterSaga(), sampleSaga()]);
+}
 
 export default rootReducer;
--- 18_redux_middleware/src/modules/sample.js
+++ 18_redux_middleware/src/modules/sample.js
@@ -1,60 +1,28 @@
-import { handleActions } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
 import * as api from '../lib/api';
-import createRequestThunk from '../lib/createRequestThunk';
+import createRequestSaga from '../lib/createRequestSaga';
 
-// 액션 타입을 선언한다
-// 한 요청당 세 개를 만들어야 한다
-
+// 액션 타입을 선언
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
 
 const GET_USERS = 'sample/GET_USERS';
 const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
 
-// thunk 함수를 생성한다
-// thunk 함수 내부에서는 시작할 때, 성공했을 때, 실패했을 때 다른 액션을 디스패치한다
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
 
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
-// export const getPost = (id) => async (dispatch) => {
-//   dispatch({ type: GET_POST }); // 요청을 시작한 것을 알림
-//   try {
-//     const response = await api.getPost(id);
-//     dispatch({
-//       type: GET_POST_SUCCESS,
-//       payload: response.data,
-//     }); // 요청 성공
-//   } catch (e) {
-//     dispatch({
-//       type: GET_POST_FAILURE,
-//       payload: e,
-//       error: true,
-//     }); // 에러 발생
-//     throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
-//   }
-// };
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
-// export const getUsers = () => async (dispatch) => {
-//   dispatch({ type: GET_USERS }); // 요청을 시작한 것을 알림
-//   try {
-//     const response = await api.getUsers();
-//     dispatch({
-//       type: GET_USERS_SUCCESS,
-//       payload: response.data,
-//     });
-//   } catch (e) {
-//     dispatch({
-//       type: GET_USERS_FAILURE,
-//       payload: e,
-//       error: true,
-//     });
-//     throw e; // 나중에 컴포넌트단에서 에러를 조회할 수 있게 해 줌
-//   }
-// };
-
-// 초기 상태를 선언함
-// 요청의 로딩 중 상태는 loading이라는 객체에서 관리함
+// 초기 상태를 선언
+// 요청의 로딩 중 상태는 loading이라는 객체에서 관리
 
 const initialState = {
   post: null,
